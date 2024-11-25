edefb5ef0647d21ca82df8337da628e681595e08
jorgee <jorge.ejarque@seqera.io>
2024-11-25 11:54:32 +0100

    Fix Java 17 build and tests issues
    Signed-off-by: jorgee <jorge.ejarque@seqera.io>

--- src/main/groovy/groovyx/gpars/extra166y/Ops.java
+++ src/main/groovy/groovyx/gpars/extra166y/Ops.java
@@ -69,7 +69,7 @@ import java.util.Comparator;
  *
  * </ul>
  *
- * <table border=1 cellpadding=0 cellspacing=0 > <caption>result = op(a) or
+ * <table border=1 > <caption>result = op(a) or
  * result = op(a,b)</caption>
  * <tr>
  * <td >
